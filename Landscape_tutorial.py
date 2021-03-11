ac3e348ded71f2a575e8454eca9c6ef8e39e7d98
Zhou (Joe) Yu <80474660+Zhou-Joe-Yu@users.noreply.github.com>
2021-03-11 15:11:44 -0600

    Update and rename Landscape_tutorial to Landscape_tutorial.py

--- Landscape_tutorial.py
+++ Landscape_tutorial.py
@@ -6,8 +6,8 @@ import pandas as pd
 import sklearn.linear_model
 
 # Load the data
-oecd_bli = pd.read_csv(datapath + "oecd_bli_2015.csv", thousands=',')
-gdp_per_capita = pd.read_csv(datapath + "gdp_per_capita.csv",thousands=',',delimiter='\t',
+oecd_bli = pd.read_csv("oecd_bli_2015.csv", thousands=',')
+gdp_per_capita = pd.read_csv("gdp_per_capita.csv",thousands=',',delimiter='\t',
                              encoding='latin1', na_values="n/a")
 
 # Prepare the data
